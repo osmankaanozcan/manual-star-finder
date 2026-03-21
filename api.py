328e504b53301810e5854df6ed58ba4fe497f498
osman_kaan_ozcan <osmankaan.ozcan@ogr.iu.edu.tr>
2026-03-21 19:37:30 +0300

    Update api.py

--- api.py
+++ api.py
@@ -10,7 +10,6 @@ from datetime import timedelta
 from astroquery.simbad import Simbad
 import re
 import traceback
-from dotenv import load_dotenv
 import os
 
 #uvicorn api:app --reload
@@ -528,6 +527,8 @@ def get_spectroscopy(target_name: str):
     except Exception as e:
         traceback.print_exc()
         return {"error": str(e)}
+import uvicorn
+
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("api:app", host="127.0.0.1", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
